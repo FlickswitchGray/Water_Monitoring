fc21bf40862577ce3e9490c345ad2cc673ed9d16
Flickswitch <123268593+FlickswitchGray@users.noreply.github.com>
2025-01-15 17:49:49 +0000

    Init commit

---
+++ WIMS_Mon_Sites.R
@@ -0,0 +1,41 @@
+library(jsonlite)
+library(httr)
+library(sf)
+library(tidyverse)
+library(magrittr)
+library(leaflet)
+
+#Find WIMS ref page here: https://environment.data.gov.uk/water-quality/view/doc/reference
+# Just sampling sites
+  
+  base_url <- "http://environment.data.gov.uk/water-quality/"
+  ending <- "id/sampling-point?_limit=9999999&area=6-28"   #6-28 is the area code for Wessex, find other areas here: https://environment.data.gov.uk/water-quality/id/ea-area.html
+  url <- paste0(base_url,ending)
+
+#Load in the api url using the httr and json packages. 
+  A_stations <- GET(url) 
+  A_stations$status_code  #if 200 all working :)
+  
+  api_char <- rawToChar(A_stations$content)
+  api <- fromJSON(api_char, flatten=T)                             #change it from json into R readable data
+  api <- api$items 
+  str(api) #Check data type so we know what we're working with, our api is type dataframe
+
+# Load in area you wish to filter by
+  BA <- read_sf("/dbfs/mnt/lab/unrestricted/harry.gray@environment-agency.gov.uk/Interim_WFD_2022.shp")  %>% st_transform(4326)
+
+#Filter 
+  BA %<>% filter(MNCAT_NAME=="Avon Bristol and Somerset North Streams") %>% st_union()
+  api_sf <-st_as_sf(api, coords=c("long","lat"), crs=4326)
+
+#Spatial crop
+  api_sf <- api_sf[BA,]
+
+# Visual check
+  leaflet (api_sf) %>% addProviderTiles(providers$Esri) %>% addCircleMarkers()
+
+
+  
+  api_sf$notation
+  
+  
